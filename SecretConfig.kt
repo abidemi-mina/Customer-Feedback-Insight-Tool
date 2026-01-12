ef906836cf732955a660d897cb8367000407d74a
abidemi-mina <79105741+abidemi-mina@users.noreply.github.com>
2026-01-12 17:11:19 +0100

    Add files via upload

---
+++ SecretConfig.kt
@@ -0,0 +1,12 @@
+package com.mina.customerinsight
+
+object SecretConfig {
+    // Get your key from https://aistudio.google.com/app/apikey
+    const val GEMINI_API_KEY = "AIzaSyDGX8CONGfXP2YM5UZDPUWg8kHMnlBH4DA"
+
+    // Base URL for the Gemini API
+    const val BASE_URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent"
+
+}
+
+
